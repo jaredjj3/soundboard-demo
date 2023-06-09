c5581b6741a70e9c19c238d6506e36563016a4cc
jaredjj3 <jared@jaredjohnson.dev>
2023-06-09 16:24:51 -0400

    Create Soundboard component

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,12 @@
 import * as React from 'react';
 import './style.css';
+import { Soundboard } from './Soundboard';
 
 export default function App() {
   return (
     <div>
-      <h1>Hello StackBlitz!</h1>
-      <p>Start editing to see some magic happen :)</p>
+      <h1>Code Next Soundboard</h1>
+      <Soundboard />
     </div>
   );
 }
---
+++ src/Soundboard.tsx
@@ -0,0 +1,5 @@
+import * as React from 'react';
+
+export const Soundboard = () => {
+  return <div>Soundboard</div>;
+};
