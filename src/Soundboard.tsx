eaf34aad546a8c5d395046c4b65b27027b9d096d
jaredjj3 <jared@jaredjohnson.dev>
2023-06-09 16:32:54 -0400

    Render dummy sounds in <Soundboard>

--- src/Soundboard.tsx
+++ src/Soundboard.tsx
@@ -1,5 +1,44 @@
 import * as React from 'react';
+import { Sound } from './types';
+
+const SOUNDS: Sound[] = [
+  {
+    name: 'foo',
+    url: '',
+  },
+  {
+    name: 'bar',
+    url: '',
+  },
+  {
+    name: 'baz',
+    url: '',
+  },
+];
 
 export const Soundboard = () => {
-  return <div>Soundboard</div>;
+  return (
+    <div>
+      <table className="table">
+        <thead>
+          <tr>
+            <th scope="col">name</th>
+            <th scope="col"></th>
+          </tr>
+        </thead>
+        <tbody>
+          {SOUNDS.map((sound) => (
+            <tr>
+              <td>{sound.name}</td>
+              <td>
+                <button type="button" className="btn btn-success">
+                  play
+                </button>
+              </td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </div>
+  );
 };
